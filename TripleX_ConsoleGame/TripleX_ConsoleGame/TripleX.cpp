486ea17813c471dd76fe870c083da24c83500f92
Abrar Akhand <Jahin345@gmail.com>
2019-12-28 17:51:57 -0700

    added .gitignore file

---
+++ TripleX_ConsoleGame/TripleX_ConsoleGame/TripleX.cpp
@@ -0,0 +1,18 @@
+/*
+git user name: Triple3Apple
+
+*/
+
+// preprocessor directive
+#include <iostream>
+
+
+int main()
+{
+	
+	std::cout << "You are a secret agent breaking into a secure server room...\n";
+	std::cout << "Enter the secret code to continue...\n";
+
+
+	return 0;
+}
